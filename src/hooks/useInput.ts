c514f0670444a55f19c19bcda2d1d48fe4e21afa
kwakhyun <khyun2348@naver.com>
2022-10-10 04:16:24 +0900

    Update: return type 분리

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -6,18 +6,21 @@ import {
   SetStateAction,
 } from "react";
 
-export const useInput = <T = any>(
-  initialData: T
-): [
+type ReturnTypes<T> = [
   T,
   (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
   Dispatch<SetStateAction<T>>
-] => {
+];
+
+export const useInput = <T>(initialData: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialData);
 
-  const handler = useCallback((e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setValue(e.target.value as unknown as T);
-  }, []);
+  const handler = useCallback(
+    (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setValue(e.target.value as unknown as T);
+    },
+    []
+  );
 
   return [value, handler, setValue];
 };
