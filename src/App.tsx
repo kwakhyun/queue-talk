df5ff83f4f1603fce132c92cdd7010bc6b15c02f
kwakhyun <khyun2348@naver.com>
2022-12-29 00:41:06 +0900

    Rename: change router component path and name

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
-import { Router } from "./router/Router";
+import { PageRouter } from "./routes";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Router />
+      <PageRouter />
     </BrowserRouter>
   );
 };
